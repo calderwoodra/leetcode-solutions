bf12f0f96756d2489276915dd118cc2c5a07bfec
Allen Calderwood <6539032+calderwoodra@users.noreply.github.com>
2021-07-24 21:55:59 -0700

    Time: 1 ms (22.63%), Space: 40.2 MB (11.59%) - LeetHub

---
+++ richest-customer-wealth/richest-customer-wealth.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int maxSum = -1;
+        for (int i = 0; i < accounts.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < accounts[i].length; j++) {
+                sum += accounts[i][j];
+            }
+            
+            maxSum = Math.max(maxSum, sum);
+        }
+        return maxSum;
+    }
+}
